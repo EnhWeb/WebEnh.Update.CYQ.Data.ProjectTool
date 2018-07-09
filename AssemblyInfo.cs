8c3f248780fc2aab51afdf8d1439f73b09e346cb
闵文辉 <mwh0808@qq.com>
2018-07-09 12:00:50 +0800

    update 修改为 .net 4

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,17 +1,17 @@
 ﻿using System.Reflection;
 // Assembly CYQ.Data.ProjectTool, Version 1.6.0.0
 
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: System.Runtime.InteropServices.Guid("3abf2ac0-6e4b-4a67-b93d-be286a9ce58f")]
 [assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: AssemblyTitle("CYQ.Data 数据层(ORM)框架 项目配置工具 QQ:101060493,263646469")]
 [assembly: AssemblyCopyright("版权所有 (C) 秋式软件 2010-2020 QQ:101060493,263646469")]
 [assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
-[assembly: AssemblyTrademark("CYQ.Data.ProjectTool QQ:101060493,263646469")]
+[assembly: AssemblyTrademark("CYQ.Data.ProjectTool QQ:101060493,263646469 .net Framework 4")]
 [assembly: System.Reflection.AssemblyCompany("秋式软件 （2016-12-03）（WebEnh Update）QQ:101060493,263646469")]
 [assembly: AssemblyDescription("论坛：http://www.cyqdata.com/cyqdata QQ:101060493,263646469")]
 [assembly: AssemblyProduct("CYQ.Data 数据层(ORM)框架 项目配置工具 QQ:101060493,263646469")]
 [assembly: System.Reflection.AssemblyConfiguration("")]
-[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyVersion("3.0.*")]
 
